99c7507d245c6e8b09c256d5c6f7f4222065a155
Benjamin <benjamin.guy.thomas@gmail.com>
2023-04-08 19:52:39 +0200

    Add RandomDistribution

---
+++ 00_intro/I2_random_distribution/java/src/main/java/org/example/Sketch.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+    private int[] randomCounts;
+
+    @Override
+    public void settings() {
+        size(640, 240);
+    }
+
+    @Override
+    public void setup() {
+        windowTitle("Random Distribution");
+        randomCounts = new int[20];
+    }
+
+    private void update() {
+        int idx = (int) random(randomCounts.length);
+        randomCounts[idx]++;
+    }
+
+    @Override
+    public void draw() {
+        update();
+
+        background(255);
+        stroke(0);
+        fill(175);
+
+        int rectWidth = width / randomCounts.length;
+        for (int i = 0; i < randomCounts.length; i++) {
+            int rectHeight = randomCounts[i];
+            int x = i * rectWidth;
+            int y = height - rectHeight;
+            rect(x, y, rectWidth, rectHeight);
+        }
+    }
+}
