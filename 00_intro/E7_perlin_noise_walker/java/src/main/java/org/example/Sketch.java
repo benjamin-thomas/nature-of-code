198facf4caa0f2681ff0c4ec6b507428c67c96a4
Benjamin <benjamin.guy.thomas@gmail.com>
2023-04-13 07:22:00 +0200

    Make longer finite trail

--- 00_intro/E7_perlin_noise_walker/java/src/main/java/org/example/Sketch.java
+++ 00_intro/E7_perlin_noise_walker/java/src/main/java/org/example/Sketch.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class Sketch extends PApplet {
 
@@ -9,20 +10,27 @@ public class Sketch extends PApplet {
     @Override
     public void settings() {
         size(640, 360);
+        fullScreen();
     }
 
     @Override
     public void setup() {
         windowTitle("Perlin Noise Walker");
-        background(0); // black
         walkers = new Walker[]{
                 new Walker(0f, 10_000f),
                 new Walker(15_000f, 25_000f),
+                new Walker(30_000f, 40_000f),
+                new Walker(45_000f, 55_000f),
+                new Walker(60_000f, 70_000f),
         };
     }
 
     @Override
     public void draw() {
+        background(0); // clear previous renders
+        stroke(255);
+        strokeWeight(2);
+
         for (Walker walker : walkers) {
             walker.step();
             walker.render();
@@ -30,39 +38,53 @@ public class Sketch extends PApplet {
     }
 
     class Walker {
+        private int cursor;
         private float tx, ty;
-        private float x, y;
-        private float prevX, prevY;
+        private final PVector[] points = new PVector[25]; // adjust trail size here
 
         Walker(float tx, float ty) {
             this.tx = tx;
             this.ty = ty;
-            x = map(noise(tx), 0, 1, 0, width);
-            y = map(noise(ty), 0, 1, 0, height);
+            this.cursor = 0;
+            step();
         }
 
         void step() {
-            prevX = x;
-            prevY = y;
+            float x = map(noise(tx), 0, 1, 0, width);
+            float y = map(noise(ty), 0, 1, 0, height);
 
-            x = map(noise(tx), 0, 1, 0, width);
-            y = map(noise(ty), 0, 1, 0, height);
+            points[this.cursor] = new PVector(x, y);
+            this.cursor++;
 
-            tx += 0.01f;
-            ty += 0.01f;
+
+            // Clear trail once buffer has been filled.
+            if (this.cursor >= points.length) {
+                // shift all by one (clear first data point)
+                for (int i = 1; i < points.length; i++) {
+                    points[i - 1] = points[i];
+                }
+                // backtrack
+                this.cursor--;
+            }
+
+            tx += 0.005f;
+            ty += 0.005f;
         }
 
         void render() {
-            stroke(255);
-            line(prevX, prevY, x, y);
+            for (int i = 1; i < points.length; i++) {
+                PVector v = points[i];
+                if (v == null) break; // buffer not filled yet
 
-            // Make trail finite
-            noStroke();
-            fill(0, 30);
-            rect(0, 0, width, height);
+                PVector prev = points[i - 1];
 
-            // Really clear. A faint trail remains indefinitely otherwise.
-            if (frameCount % 60 == 0) background(0);
+                // Make the trail fainter and fainter
+                float progression = i / ((float) points.length);
+                float alpha = 255 * progression;
+                stroke(255, 255, 255, alpha);
+
+                line(prev.x, prev.y, v.x, v.y);
+            }
         }
 
     }
