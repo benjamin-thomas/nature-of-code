58288226b5d6179430061b6a0c436c0ee3aebce2
Benjamin <benjamin.guy.thomas@gmail.com>
2023-04-17 08:41:12 +0200

    C: add Perlin noise walker

---
+++ 00_intro/E7_perlin_noise_walker/c/raylib/main.c
@@ -0,0 +1,96 @@
+/*
+ 2 options to build/run:
+
+     1. Use cmake only
+
+         cmake -B build
+         cmake --build ./build
+         ./build/RaylibProject
+
+     2. Use cmake + ninja (Clion style)
+
+        cmake -B cmake-build-debug/
+        ninja -C cmake-build-debug
+        cmake-build-debug/RaylibProject
+ */
+
+#include <stdio.h>
+#include "raylib.h"
+#include "noise1234.h"
+
+// A static global variable or a function is "seen" only in the file it's declared in
+static const int
+        screenWidth = 800,
+        screenHeight = 450;
+
+static int cursor = 0;
+
+static const float
+        SPEED = 0.015f;
+
+static float
+        tx = 0, ty = 10000;
+
+
+#define TRAIL_SIZE 50
+static Vector2 points[TRAIL_SIZE];
+
+Vector2 update() {
+    float x = noise1(tx) * (float) screenWidth / 2;
+    x += (float) screenWidth / 2;
+
+    float y = noise1(ty) * (float) screenHeight / 2;
+    y += (float) screenHeight / 2;
+
+    tx += SPEED;
+    ty += SPEED;
+    Vector2 v = {x, y};
+    points[cursor] = v;
+    cursor++;
+
+    // Remove old points once buffer has been filled (= trail)
+    if (cursor >= TRAIL_SIZE) {
+        // shift all by one
+        for (int i = 1; i < TRAIL_SIZE; ++i) {
+            points[i - 1] = points[i];
+        }
+        // backtrack
+        cursor--;
+    }
+
+    return v;
+}
+
+void draw(Vector2 v2) {
+    ClearBackground(BLACK);
+
+    for (int i = 1; i < TRAIL_SIZE; ++i) {
+        Vector2 v = points[i];
+        Vector2 prev = points[i - 1];
+
+        if (v.x == 0.0f && v.y == 0.0f) break; // buffer not filled yet
+
+        float progression = (float) i / (float) TRAIL_SIZE;
+        Color color = ColorAlpha(WHITE, progression);
+        DrawLineEx(prev, v, 2.0f, color);
+    }
+}
+
+int main() {
+    InitWindow(screenWidth, screenHeight, "C/Raylib: Perlin Noise Walker");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        Vector2 v = update();
+        draw(v);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
+
+
+
