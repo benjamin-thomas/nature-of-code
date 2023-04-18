a8d74dcbb991c0d49c89c5fd8de4a97f67291934
Benjamin <benjamin.guy.thomas@gmail.com>
2023-04-18 19:54:02 +0200

    Finalize C Perlin noise walkers

--- 00_intro/E7_perlin_noise_walker/c/raylib/main.c
+++ 00_intro/E7_perlin_noise_walker/c/raylib/main.c
@@ -14,7 +14,7 @@
         cmake-build-debug/RaylibProject
  */
 
-#include <stdio.h>
+#include <stdlib.h>
 #include "raylib.h"
 #include "noise1234.h"
 
@@ -23,10 +23,8 @@ static const int
         SCREEN_WIDTH = 800,
         SCREEN_HEIGHT = 450;
 
-static int cursor = 0;
-
 static const float
-        SPEED = 0.015f;
+        STEP_SIZE = 0.04f; // this value affects the speed (and the FPS too)
 
 #define TRAIL_SIZE 50
 
@@ -34,89 +32,75 @@ typedef struct Walker {
     float tx, ty;
     int cursor;
     struct Vector2 points[TRAIL_SIZE];
+    Color color;
 } Walker;
 
-void step_walker(Walker *walker) {
-    printf("step start: tx=%f\n", walker->tx);
-    float noiseX = noise1(walker->tx);
+void stepWalker(Walker *pWalker) {
+    float noiseX = noise1(pWalker->tx);
     float x = noiseX * (float) SCREEN_WIDTH / 2.0f;
     x += (float) SCREEN_WIDTH / 2.0f;
 
-    float noiseY = noise1(walker->ty);
+    float noiseY = noise1(pWalker->ty);
     float y = noiseY * (float) SCREEN_HEIGHT / 2.0f;
     y += (float) SCREEN_HEIGHT / 2.0f;
 
-    walker->points[walker->cursor] = (Vector2) {x, y};
-    walker->cursor++;
+    pWalker->points[pWalker->cursor] = (Vector2) {x, y};
 
-    // Remove old points once buffer has been filled (= trail)
-    if (walker->cursor >= TRAIL_SIZE) {
-        // shift all by one
-        for (int i = 0; i < TRAIL_SIZE - 1; ++i) {
-            walker->points[i] = walker->points[i + 1];
-        }
-        // backtrack
-        walker->cursor--;
-    }
-
-    walker->tx += SPEED;
-    walker->ty += SPEED;
-    printf("step end: tx=%f\n", walker->tx);
-}
-
-void update(struct Walker (*walkers)[2]) {
-    for (int i = 0; i < 2; ++i) {
-        struct Walker *w = walkers[i];
-        printf("Updating walker n°%d: tx=%f\n", i, w->tx);
-        step_walker(w);
-    }
-}
-
-void draw(struct Walker (*walkers)[2]) {
-    ClearBackground(BLACK);
-
-    for (int i = 0; i < 2; ++i) {
-        struct Walker *w = walkers[i];
-//        printf("Drawing walker n°%d: (%f, %f)\n", i, w->points[0].x, w->points[0].y);
-
-        for (int j = 1; j < TRAIL_SIZE; ++j) {
-            struct Vector2 v = w->points[j];
-            struct Vector2 prev = w->points[j - 1];
-//            printf("%d] p=%d prev=next=(%f, %f) v=(%f, %f)\n", i, j, prev.x, prev.y, v.x, v.y);
-
-            if (v.x == 0.0f && v.y == 0.0f) return; // buffer not filled yet
-
-//            float progression = (float) j / (float) TRAIL_SIZE;
-
-//            Color color;
-//            if (i == 0) {
-//                color = ColorAlpha(WHITE, progression);
-//            } else {
-//                color = ColorAlpha(RED, progression);
-//            }
-
-
-            DrawLineEx(prev, v, 2.0f, RED);
+    bool allVectorsSet = pWalker->cursor >= TRAIL_SIZE - 1;
+    if (!allVectorsSet) {
+        pWalker->cursor++;
+    } else {
+        // Shift all by one
+        for (int i = 1; i < TRAIL_SIZE; ++i) {
+            pWalker->points[i - 1] = pWalker->points[i];
         }
     }
+
+    pWalker->tx += STEP_SIZE;
+    pWalker->ty += STEP_SIZE;
 }
 
-int main() {
+void drawWalker(Walker *pWalker) {
+    for (int i = 1; i < TRAIL_SIZE; ++i) {
+        struct Vector2 v = pWalker->points[i];
+        struct Vector2 prev = pWalker->points[i - 1];
+
+        bool vectorNotSetYet = i >= pWalker->cursor;
+        if (vectorNotSetYet) break;
+
+        float progression = (float) i / (float) TRAIL_SIZE;
+        Color color = ColorAlpha(pWalker->color, progression);
+
+        DrawLineEx(prev, v, 2.0f, color);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int fps = 60;
+    if (argc > 1) {
+        char *endToken;
+        int n = (int) strtol(argv[1], &endToken, 10);
+        if (*endToken == '\0') fps = n;
+    }
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "C/Raylib: Perlin Noise Walker");
-    SetTargetFPS(60);
+    SetTargetFPS(fps);
 
     struct Walker walkers[] = {
-            {.tx = 0.0f, .ty = 10000.0f, .cursor = 0},
-            {.tx = 15000.0f, .ty = 25000.0f, .cursor = 0}
+            {.tx = 0.0f, .ty = 10000.0f, .cursor = 0, .color = RED},
+            {.tx = 15000.0f, .ty = 25000.0f, .cursor = 0, .color = ORANGE},
     };
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-//        update();
-        update(&walkers);
-        draw(&walkers);
-//        draw();
+        ClearBackground(BLACK);
+
+        for (int i = 0; i < sizeof(walkers) / sizeof(walkers[0]); ++i) {
+            struct Walker *pWalker = &walkers[i];
+            stepWalker(pWalker);
+            drawWalker(pWalker);
+        }
+
         EndDrawing();
     }
 
