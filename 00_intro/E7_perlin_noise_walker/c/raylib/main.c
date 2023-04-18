22eb22ef9f26e82a3d8e8b7eb53006367558e09d
Benjamin <benjamin.guy.thomas@gmail.com>
2023-04-18 08:26:04 +0200

    WIP C: two Perlin walkers

--- 00_intro/E7_perlin_noise_walker/c/raylib/main.c
+++ 00_intro/E7_perlin_noise_walker/c/raylib/main.c
@@ -14,74 +14,109 @@
         cmake-build-debug/RaylibProject
  */
 
+#include <stdio.h>
 #include "raylib.h"
 #include "noise1234.h"
 
 // A static global variable or a function is "seen" only in the file it's declared in
 static const int
-        screenWidth = 800,
-        screenHeight = 450;
+        SCREEN_WIDTH = 800,
+        SCREEN_HEIGHT = 450;
 
 static int cursor = 0;
 
 static const float
         SPEED = 0.015f;
 
-static float
-        tx = 0, ty = 10000;
-
-
 #define TRAIL_SIZE 50
-static Vector2 points[TRAIL_SIZE];
 
-void update() {
-    float x = noise1(tx) * (float) screenWidth / 2;
-    x += (float) screenWidth / 2;
+typedef struct Walker {
+    float tx, ty;
+    int cursor;
+    struct Vector2 points[TRAIL_SIZE];
+} Walker;
 
-    float y = noise1(ty) * (float) screenHeight / 2;
-    y += (float) screenHeight / 2;
+void step_walker(Walker *walker) {
+    printf("step start: tx=%f\n", walker->tx);
+    float noiseX = noise1(walker->tx);
+    float x = noiseX * (float) SCREEN_WIDTH / 2.0f;
+    x += (float) SCREEN_WIDTH / 2.0f;
 
-    tx += SPEED;
-    ty += SPEED;
-    points[cursor] = (Vector2) {x, y};
-    cursor++;
+    float noiseY = noise1(walker->ty);
+    float y = noiseY * (float) SCREEN_HEIGHT / 2.0f;
+    y += (float) SCREEN_HEIGHT / 2.0f;
+
+    walker->points[walker->cursor] = (Vector2) {x, y};
+    walker->cursor++;
 
     // Remove old points once buffer has been filled (= trail)
-    if (cursor >= TRAIL_SIZE) {
+    if (walker->cursor >= TRAIL_SIZE) {
         // shift all by one
-        for (int i = 1; i < TRAIL_SIZE; ++i) {
-            points[i - 1] = points[i];
+        for (int i = 0; i < TRAIL_SIZE - 1; ++i) {
+            walker->points[i] = walker->points[i + 1];
         }
         // backtrack
-        cursor--;
+        walker->cursor--;
+    }
+
+    walker->tx += SPEED;
+    walker->ty += SPEED;
+    printf("step end: tx=%f\n", walker->tx);
+}
+
+void update(struct Walker (*walkers)[2]) {
+    for (int i = 0; i < 2; ++i) {
+        struct Walker *w = walkers[i];
+        printf("Updating walker n°%d: tx=%f\n", i, w->tx);
+        step_walker(w);
     }
 }
 
-void draw() {
+void draw(struct Walker (*walkers)[2]) {
     ClearBackground(BLACK);
 
-    for (int i = 1; i < TRAIL_SIZE; ++i) {
-        Vector2 v = points[i];
-        Vector2 prev = points[i - 1];
+    for (int i = 0; i < 2; ++i) {
+        struct Walker *w = walkers[i];
+//        printf("Drawing walker n°%d: (%f, %f)\n", i, w->points[0].x, w->points[0].y);
 
-        if (v.x == 0.0f && v.y == 0.0f) break; // buffer not filled yet
+        for (int j = 1; j < TRAIL_SIZE; ++j) {
+            struct Vector2 v = w->points[j];
+            struct Vector2 prev = w->points[j - 1];
+//            printf("%d] p=%d prev=next=(%f, %f) v=(%f, %f)\n", i, j, prev.x, prev.y, v.x, v.y);
 
-        float progression = (float) i / (float) TRAIL_SIZE;
-        Color color = ColorAlpha(WHITE, progression);
-        DrawLineEx(prev, v, 2.0f, color);
+            if (v.x == 0.0f && v.y == 0.0f) return; // buffer not filled yet
+
+//            float progression = (float) j / (float) TRAIL_SIZE;
+
+//            Color color;
+//            if (i == 0) {
+//                color = ColorAlpha(WHITE, progression);
+//            } else {
+//                color = ColorAlpha(RED, progression);
+//            }
+
+
+            DrawLineEx(prev, v, 2.0f, RED);
+        }
     }
 }
 
 int main() {
     SetConfigFlags(FLAG_MSAA_4X_HINT);
-    InitWindow(screenWidth, screenHeight, "C/Raylib: Perlin Noise Walker");
-
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "C/Raylib: Perlin Noise Walker");
     SetTargetFPS(60);
+
+    struct Walker walkers[] = {
+            {.tx = 0.0f, .ty = 10000.0f, .cursor = 0},
+            {.tx = 15000.0f, .ty = 25000.0f, .cursor = 0}
+    };
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        update();
-        draw();
+//        update();
+        update(&walkers);
+        draw(&walkers);
+//        draw();
         EndDrawing();
     }
 
