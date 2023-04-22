2cfe72641439d68bcda435b53daf25aa114d1f95
Benjamin <benjamin.guy.thomas@gmail.com>
2023-04-22 08:52:00 +0200

    Document OCaml perf

--- off_track/perlin_noise/ocaml/bin/main.ml
+++ off_track/perlin_noise/ocaml/bin/main.ml
@@ -1,5 +1,20 @@
 (*
-   dune exec --display=quiet bin/main.exe && eog /tmp/white-noise.ppm
+
+dune exec --display=quiet bin/main.exe && eog /tmp/white-noise.ppm
+
+
+---
+
+The performance is slightly better than C (0m0.029s), slightly worse than Rust (0m0.010s).
+
+dune build --profile release
+
+$ time _build/default/bin/main.exe
+
+real    0m0.023s
+user    0m0.019s
+sys     0m0.004s
+
  *)
 
 let () = Noise.run ()
