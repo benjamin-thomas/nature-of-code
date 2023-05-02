8b6b101c61399a346ffe87a7be94287ac1c51e07
Benjamin <benjamin.guy.thomas@gmail.com>
2023-05-02 07:24:39 +0200

    Create bin entry point

--- off_track/perlin_noise/ocaml/bin/main.ml
+++ off_track/perlin_noise/ocaml/bin/main.ml
@@ -1,15 +1,12 @@
 (*
 
-dune exec --display=quiet bin/main.exe && eog /tmp/white-noise.ppm
-
-
----
+dune exec --display=quiet ./bin/main.exe 1 && eog /tmp/ocaml-white-noise.ppm
 
 The performance is slightly better than C (0m0.029s), slightly worse than Rust (0m0.010s).
 
 dune build --profile release
 
-$ time _build/default/bin/main.exe
+$ time _build/default/bin/main.exe 1
 
 real    0m0.023s
 user    0m0.019s
@@ -17,4 +14,38 @@ sys     0m0.004s
 
  *)
 
-let () = Noise.run ()
+let usage =
+  {|
+Usage:
+
+  ./main.exe SELECTION
+
+  Available SELECTION values:
+
+  - 1: generate a white-noise PPM image
+  - 2: open a window, demonstrating the usage of a noise 1D function applied to some random points
+
+1.
+|}
+;;
+
+type selection = GenerateWhiteNoise | GraphWithNoise1D
+
+let selection_of_int = function
+  | 1 -> GenerateWhiteNoise
+  | 2 -> GraphWithNoise1D
+  | _ -> raise @@ Invalid_argument "not a valid selection"
+;;
+
+let get_selection () =
+  try Sys.argv.(1) |> int_of_string |> selection_of_int with
+  | _ ->
+      print_endline usage
+      ; exit 1
+;;
+
+let () =
+  match get_selection () with
+  | GenerateWhiteNoise -> Noise.generate_white_noise ()
+  | GraphWithNoise1D -> print_endline "TODO"
+;;
