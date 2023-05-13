e6cd28bc8a5268d835624d80954564e4f8bb6f9a
Benjamin <benjamin.guy.thomas@gmail.com>
2023-05-13 15:25:02 +0200

    Remove obsolete impl comment

--- off_track/perlin_noise/ocaml/lib/Noise1D.ml
+++ off_track/perlin_noise/ocaml/lib/Noise1D.ml
@@ -17,14 +17,6 @@ let smooth t =
   shift *. 0.5                        (* Range:  0..1 *)
   [@@ocamlformat "disable"]
 
-(** Given an array of reference points, returns any point `x`, "smoothly" interpolated against the previous and next
-    ref points.
-
-    If there is no next ref point, the first ref point is chosen. This enables seamlessly repeating the noise pattern.
-
-    Also signal to the caller if we've "hit" a reference point. This enables the caller to draw peaks and valleys differently
-    from the rest of the interpolated points.
-  *)
 let point_at x ~ref_points ~ref_points_size =
   let lo = int_of_float x mod ref_points_size in
   let hi = (lo + 1) mod ref_points_size in
